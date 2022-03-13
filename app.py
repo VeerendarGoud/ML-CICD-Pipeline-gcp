5203edc815b39ff8cdc3b66e9bbe3e2f763829cd
veerendar2888 <veerendar2888@gmail.com>
2022-03-13 15:00:31 +0530

    changed models dir

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.text("Provide URL of Location Image for image classification")
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model = tf.keras.models.load_model('./model/models/')
+  model = tf.keras.models.load_model('./models/models/')
   return model
 
 with st.spinner('Loading Model Into Memory....'):
